52344fd6fd8bdd04f39410518d94c080b82fa2cb
git_user <admin@tradom.ca>
2015-12-06 14:51:15 -0500

    modification d'un post

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', 'blog.views.accueil', name='accueil'),
     url(r'^articles/', 'blog.views.articles', name='articles'),
     url(r'^post/(?P<pk>[0-9]+)/$', 'blog.views.post_detail', name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', 'blog.views.post_edit', name='post_edit'),
     url(r'^post/new/$', 'blog.views.post_new', name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AnonymousUser, User
+
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .forms import PostForm
@@ -7,7 +9,7 @@ from django.utils import timezone
 
 # Create your views here.
 def accueil(request):
-    return render(request, "blog/accueil.html", {})
+    return render(request, 'blog/accueil.html', {})
 
 
 def articles(request):
@@ -17,7 +19,7 @@ def articles(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request,"blog/post_detail.html",{'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
@@ -28,8 +30,23 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('blog.vieus.post_detail', pk=post.pk)
+            return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
 
-    return render(request,"blog/post_edit.html", {'form':form})
+    return render(request, 'blog/post_edit.html', {'form':form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
