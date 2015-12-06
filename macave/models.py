7c911a02ae30a38a9a0e622ca46a89281d6b59c1
git_user <admin@tradom.ca>
2015-12-06 17:07:02 -0500

    macave ajout

---
+++ macave/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Vin(models.Model):
+    author = models.ForeignKey('auth.User')
+    nom = models.CharField(max_length=50)
+    etiquette = models.CharField(max_length=30)
+    millesime = models.IntegerField()
+    commentaire = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return '{0}({1})'.format(self.etiquette, str(self.millesime))
---
+++ macave/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Vin
+
+
+# Create your views here.
+def accueil(request):
+    return render(request, 'macave/accueil.html', {})
+
+
+def vins(request):
+    vins = Vin.objects.order_by('published_date')
+    return render(request,'macave/vins.html', {'vins':vins})
+
+
+def vin_detail(request, pk):
+    vin = get_object_or_404(Vin, pk=pk)
+    return render(request, 'macave/vin_detail.html', {'vin': vin})
+
