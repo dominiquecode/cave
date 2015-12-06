859f3b02885e6375ed4609c41bbb505c8b372d54
git_user <admin@tradom.ca>
2015-12-06 12:39:36 -0500

    Initial

--- cave/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-
-urlpatterns = [
-    url(r'^$', 'blog.views.accueil', name='accueil'),
-    url(r'^articles/', 'blog.views.articles', name='articles'),
-    url(r'^post/(?P<pk>[0-9]+)/$', 'blog.views.post_detail', name='post_detail'),
-    url(r'^post/new/$', 'blog.views.post_new', name='post_new'),
-]
