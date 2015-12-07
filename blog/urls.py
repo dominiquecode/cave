94a8ced6953fb928a71d5f4e14f1bbf45d24b507
git_user <admin@tradom.ca>
2015-12-07 08:43:43 -0500

    vérifications post

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     url(r'^articles/', 'blog.views.articles', name='articles'),
     url(r'^post/(?P<pk>[0-9]+)/$', 'blog.views.post_detail', name='post_detail'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', 'blog.views.post_edit', name='post_edit'),
+    url(r'^post/(?P<pk>[0-9]+)/publish/$', 'blog.views.post_publish', name='post_publish'),
     url(r'^post/new/$', 'blog.views.post_new', name='post_new'),
+    url(r'^drafts/$', 'blog.views.post_draft_list', name='post_draft_list'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render_to_response
+
 from django.contrib.auth.models import AnonymousUser, User
 
 from django.shortcuts import render, get_object_or_404
@@ -28,7 +30,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -50,3 +51,25 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+
+
+
+
