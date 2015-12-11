6db24d0ddfc41004f23eec0ee6bfd24cdcb23be2
Dominique <admin@leboga.net>
2015-12-11 12:20:23 -0500

    ajout des fixtures de base

--- macave/migrations/0001_initial.py
+++ macave/migrations/0001_initial.py
@@ -13,8 +13,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Pays',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('nom_pays', models.CharField(max_length=20, unique=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('nom_pays', models.CharField(unique=True, max_length=20)),
                 ('commentaire', models.TextField()),
             ],
             options={
@@ -24,23 +24,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Region',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('nom_region', models.CharField(max_length=20, unique=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('nom_region', models.CharField(unique=True, max_length=20)),
                 ('commentaire', models.TextField()),
-                ('nom_pays', models.ForeignKey(null=True, to='macave.Pays', to_field='nom_pays', blank=True)),
+                ('nom_pays', models.ForeignKey(to='macave.Pays', to_field='nom_pays', null=True, blank=True)),
             ],
         ),
         migrations.CreateModel(
             name='Vin',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('nom_vin', models.CharField(max_length=50)),
                 ('etiquette', models.CharField(max_length=30)),
                 ('millesime', models.IntegerField()),
                 ('commentaire', models.TextField()),
                 ('created_date', models.DateTimeField(auto_now_add=True)),
-                ('published_date', models.DateTimeField(null=True, auto_now=True)),
-                ('nom_region', models.ForeignKey(null=True, to='macave.Region', to_field='nom_region', blank=True)),
+                ('published_date', models.DateTimeField(auto_now=True, null=True)),
+                ('nom_region', models.ForeignKey(to='macave.Region', to_field='nom_region', null=True, blank=True)),
             ],
         ),
     ]
